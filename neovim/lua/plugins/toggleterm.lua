08b2a0ac42d63da6900856fe9f69641901f1cfe9
ryotakato <ryotakato4@gmail.com>
2025-09-02 17:45:13 +1200

    delete todotxt & add config for todotui

--- neovim/lua/plugins/toggleterm.lua
+++ neovim/lua/plugins/toggleterm.lua
@@ -4,11 +4,34 @@ return {
   keys = {
     { '<leader>t', '<cmd>ToggleTerm<cr>', desc = 'ToggleTerm' },
   },
-  opts = {
-    
-  },
   config = function() 
-    require("toggleterm").setup()
+    require("toggleterm").setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 20
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.4
+        end
+      end,
+      start_in_insert = false,
+      direction = 'vertical',
+    })
+
+    local Terminal  = require('toggleterm.terminal').Terminal
+    local todotui = Terminal:new({ 
+      cmd = "todotui ~/todotui/todo.txt", 
+      hidden = true, 
+      direction = "float", 
+      on_open = function()
+        vim.cmd('startinsert!')
+      end
+    })
+
+    function _todotui_toggle()
+      todotui:toggle()
+    end
+
+    vim.api.nvim_set_keymap("n", "<leader>td", "<cmd>lua _todotui_toggle()<CR>", { noremap = true, silent = true})
 
     --local Terminal  = require('toggleterm.terminal').Terminal
     --local yazi = Terminal:new({ cmd = "yazi", hidden = true, direction = "float", })
